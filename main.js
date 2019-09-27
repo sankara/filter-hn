4fef06a5875d8595742c420cd786330d4f8e1742
Sankara Rameswaran <hello@sankara.me>
2019-09-27 13:04:47 -0700

    Adding a hide link

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
-(function($) {
-    browser.storage.sync.get().then(function(settings) {
-        hideUnsavoryCommenters(settings.hiddenUsers);
+console.log("main.js");
+
+var closuresToRun = [];
+
+Element.prototype.appendAfter = function (element) {
+    element.parentNode.insertBefore(this, element.nextSibling);
+},false;
+
+function runOnHNLinks() {
+    var matchedLinks = document.querySelectorAll(".hnuser");
+    for(var i = 0; i < matchedLinks.length; i++) {
+        console.log("Running closures on: " + matchedLinks[i]);
+        for(var j = 0; j < closuresToRun.length; j++) {
+            closuresToRun[j](matchedLinks[i]);
+        }
+    }
+}
+
+function registerToRunOnHNLinks(closure) {
+    closuresToRun.push(closure);
+}
+
+function hideUnsavoryCommenters(hiddenUsers) {
+    registerToRunOnHNLinks((link) => {
+        console.log("Checking to hide comment: " + link);
+        if(hiddenUsers.includes(link.textContent))
+            link.closest("tr.comtr").className += "noshow coll";
     });
+    //recoll();
+}
 
-    function hideUnsavoryCommenters(hiddenUsers) {
-        $(".hnuser").each(function() {
-            $this = $(this);
-            if(!hiddenUsers.includes($this.text()))
-                return;
-            $this.parents("tr.comtr")
-                //.addClass("coll")
-                .addClass("noshow");
-        });
-        //recoll();
-    }
+function hideUser(user) {
+    browser.storage.sync.get().then((settings) => {
+        settings.hiddenUsers.push(user);
+        browser.storage.sync.set(settings);
+        hideUnsavoryCommenters(settings.hiddenUsers);
+        runOnHNLinks();
+    });
+}
 
-    function addHideLinks() {
-        $(".hnuser")
-    }
+function addHideLink() {
+    registerToRunOnHNLinks((link) => {
+        console.log("Adding hide link to: " + link);
+        var hideLink = document.createElement("a");
+        hideLink.innerHTML = " (hide) ";
+        hideLink.style.cursor = "pointer";
+        hideLink.onclick = (e) => {
+            hideUser(link.textContent);
+        };
+        hideLink.appendAfter(link);
+    });
+}
 
-})(jQuery);
+function onLoad() {
+    console.log("Executing onLoad");
+    browser.storage.sync.get().then((settings) => {
+        hideUnsavoryCommenters(settings.hiddenUsers);
+        addHideLink();
+        runOnHNLinks();
+    });
+}
+
+onLoad();
